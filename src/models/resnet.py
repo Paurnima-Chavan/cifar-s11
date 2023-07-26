7cb4fd933c6b386eada035f267aada43898df4e3
Paurnima Chavan <paurnimach@gmail.com>
2023-07-26 20:18:25 +0530

    Update resnet.py

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -5,6 +5,7 @@ ResNet in PyTorch.
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
 
 
 class BasicBlock(nn.Module):
@@ -72,6 +73,16 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
+
+def model_summary(model, input_size):
+    """
+    This function displays a summary of the model, providing information about its architecture,
+    layer configuration, and the number of parameters it contains.
+    :param model: model
+    :param input_size: input_size for model
+    :return:
+    """
+    summary(model, input_size=input_size)
 
 def test():
     net = ResNet18()
